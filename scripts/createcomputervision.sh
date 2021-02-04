d11503b5c7f1eecc662125242af6d40908282e66
Glenn Musa <glennmusa@users.noreply.github.com>
2021-02-03 21:43:07 -0500

    Gmusa/createcomputervision (#3)
    * add createcomputervision.sh to create required resources
    
    * include a newline

---
+++ scripts/createcomputervision.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+PGM=`basename $0`
+
+if ! command -v az &> /dev/null
+then
+    echo "${PGM} Azure CLI could not be found. This script requires Azure CLI."
+    echo "${PGM} Install instructions here: https://docs.microsoft.com/en-us/cli/azure/install-azure-cli"
+    exit
+fi
+
+rgName=${1:-dchelicopterwatchrg}
+rgLocation=${2:-eastus}
+
+timestamp=$(date +%s)
+deploymentName="${rgName}${timestamp}"
+
+az group create \
+    --name "$rgName" \
+    --location "$rgLocation" \
+
+az deployment group create \
+    --name "$deploymentName" \
+    --resource-group "$rgName" \
+    --template-uri "https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/101-cognitive-services-Computer-vision-API/azuredeploy.json"
+
+cogsAccountName=$(az deployment group show \
+    --name "$deploymentName" \
+    --resource-group "$rgName" \
+    --query properties.parameters.accountName.value \
+    --output tsv)
+
+cogsAccountKey=$(az cognitiveservices account keys list \
+    --name "$cogsAccountName" \
+    --resource-group "$rgName" \
+    --query "key1" \
+    --output tsv)
+
+echo "Created a Cognitive Services Account: ${cogsAccountName}"
+echo "Cognitive Services Account key: ${cogsAccountKey}"
