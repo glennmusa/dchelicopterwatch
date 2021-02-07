cf2848e3a0045f3c14c92bddbd541e85058d7147
Glenn Musa <glennmusa@users.noreply.github.com>
2021-02-07 02:26:40 +0000

    change samples

--- README.md
+++ README.md
@@ -31,7 +31,7 @@ An app that tries to identify the helicopters of Washington DC
 
     <https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/101-cognitive-services-Computer-vision-API/azuredeploy.json>
 
-1. Run quickstart.py
+1. Run quickstart.py from the terminal
 
     ```python
     python quickstart.py
@@ -46,3 +46,74 @@ An app that tries to identify the helicopters of Washington DC
     The full Azure-Samples/cognitive-services-quickstart-code:
 
     <https://github.com/Azure-Samples/cognitive-services-quickstart-code/blob/master/python/ComputerVision/ComputerVisionQuickstart.py>
+
+1. Inspect results in the terminal
+
+    You'll see what the Computer Vision API returns similar to this:
+
+    ```plaintext
+    ===== Describe an Image =====
+    Description of local image: 
+    'a helicopter flying in the sky' with confidence 59.82%
+
+    ===== Categorize an Image =====
+    Categories from local image: 
+    'sky_object' with confidence 99.61%
+
+    ===== Tag an Image =====
+    Tags in the local image: 
+    'sky' with confidence 100.00%
+    'outdoor' with confidence 99.77%
+    'transport' with confidence 99.01%
+    'aircraft' with confidence 97.01%
+    'helicopter' with confidence 90.45%
+    'vehicle' with confidence 86.20%
+    'air' with confidence 74.26%
+    'airplane' with confidence 57.47%
+    'blue' with confidence 56.61%
+    'helicopter rotor' with confidence 55.10%
+    'engine' with confidence 20.81%
+    'fighter' with confidence 13.07%
+    'landing' with confidence 11.86%
+
+    ===== Detect Color =====
+    Getting color scheme of the local image: 
+    Is black and white: False
+    Accent color: 2A62A1
+    Dominant background color: Grey
+    Dominant foreground color: Grey
+    Dominant colors: []
+
+    ===== Detect Objects =====
+    Detecting objects in local image:
+    object at location 1779, 2674, 754, 1253
+    object at location 99, 2269, 143, 1389
+
+    ===== Generate Thumbnail =====
+    Generating thumbnail from a local image...
+    Thumbnail saved to local folder.
+    ```
+
+1. Try different source images
+
+    Try different images from the /resources directory and see what results you get by updating [quickstart.py line 14](quickstart.py)
+
+    ```python
+    local_image_path = "resources/helicopter.jpg"
+    ```
+
+    ```python
+    local_image_path = "resources/plane.jpg"
+    ```
+
+    ```python
+    local_image_path = "resources/birds.jpg"
+    ```
+
+    ```python
+    local_image_path = "resources/faces.jpg"
+    ```
+
+    ```python
+    local_image_path = "resources/printed_text.jpg"
+    ```
--- quickstart.py
+++ quickstart.py
@@ -11,7 +11,7 @@ import time
 
 from dotenv import load_dotenv
 
-local_image_path = "resources/faces.jpg"
+local_image_path = "resources/helicopter.jpg"
 
 load_dotenv()
 subscription_key = os.getenv('dchwcogsaccountkey')
@@ -23,7 +23,7 @@ computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredenti
 Describe an Image - local
 This example describes the contents of an image with the confidence score.
 '''
-print("===== Describe an Image - local =====")
+print("===== Describe an Image =====")
 
 local_image = open(local_image_path, "rb")
 
@@ -44,7 +44,7 @@ END - Describe an Image - local
 Categorize an Image -  local
 This example extracts categories from a local image with a confidence score
 '''
-print("===== Categorize an Image - local =====")
+print("===== Categorize an Image =====")
 
 local_image = open(local_image_path, "rb")
 local_image_features = ["categories"]
@@ -66,7 +66,7 @@ END - Categorize an Image - local
 Tag an Image - local
 This example returns a tag (key word) for each thing in the image.
 '''
-print("===== Tag an Image - local =====")
+print("===== Tag an Image =====")
 
 local_image = open(local_image_path, "rb")
 
@@ -84,56 +84,10 @@ END - Tag an Image - local
 '''
 
 '''
-Detect Faces - local
-This example detects faces in a local image, gets their gender and age, 
-and marks them with a bounding box.
-'''
-print("===== Detect Faces - local =====")
-
-local_image = open(local_image_path, "rb")
-local_image_features = ["faces"]
-
-detect_faces_results_local = computervision_client.analyze_image_in_stream(local_image, local_image_features)
-
-print("Faces in the local image: ")
-if (len(detect_faces_results_local.faces) == 0):
-    print("No faces detected.")
-else:
-    for face in detect_faces_results_local.faces:
-        print("'{}' of age {} at location {}, {}, {}, {}".format(face.gender, face.age, \
-        face.face_rectangle.left, face.face_rectangle.top, \
-        face.face_rectangle.left + face.face_rectangle.width, \
-        face.face_rectangle.top + face.face_rectangle.height))
-print()
-'''
-END - Detect Faces - local
-'''
-
-'''
-Detect Adult or Racy Content - local
-This example detects adult or racy content in a local image, then prints the adult/racy score.
-The score is ranged 0.0 - 1.0 with smaller numbers indicating negative results.
-'''
-print("===== Detect Adult or Racy Content - local =====")
-
-local_image = open(local_image_path, "rb")
-local_image_features = ["adult"]
-
-detect_adult_results_local = computervision_client.analyze_image_in_stream(local_image, local_image_features)
-
-print("Analyzing local image for adult or racy content ... ")
-print("Is adult content: {} with confidence {:.2f}".format(detect_adult_results_local .adult.is_adult_content, detect_adult_results_local .adult.adult_score * 100))
-print("Has racy content: {} with confidence {:.2f}".format(detect_adult_results_local .adult.is_racy_content, detect_adult_results_local .adult.racy_score * 100))
-print()
-'''
-END - Detect Adult or Racy Content - local
-'''
-
-'''
 Detect Color - local
 This example detects the different aspects of its color scheme in a local image.
 '''
-print("===== Detect Color - local =====")
+print("===== Detect Color =====")
 
 local_image = open(local_image_path, "rb")
 local_image_features = ["color"]
@@ -152,78 +106,12 @@ END - Detect Color - local
 '''
 
 '''
-Detect Domain-specific Content - local
-This example detects celebrites and landmarks in local images.
-'''
-print("===== Detect Domain-specific Content - local =====")
-
-local_image = open(local_image_path, "rb")
-
-detect_domain_results_celebs_local = computervision_client.analyze_image_by_domain_in_stream("celebrities", local_image)
-
-print("Celebrities in the local image:")
-if len(detect_domain_results_celebs_local.result["celebrities"]) == 0:
-    print("No celebrities detected.")
-else:
-    for celeb in detect_domain_results_celebs_local.result["celebrities"]:
-        print(celeb["name"])
-
-local_image_path_landmark = "resources/landmark.jpg"
-local_image_landmark = open(local_image_path_landmark, "rb")
-
-detect_domain_results_landmark_local = computervision_client.analyze_image_by_domain_in_stream("landmarks", local_image_landmark)
-print()
-
-print("Landmarks in the local image:")
-if len(detect_domain_results_landmark_local.result["landmarks"]) == 0:
-    print("No landmarks detected.")
-else:
-    for landmark in detect_domain_results_landmark_local.result["landmarks"]:
-        print(landmark["name"])
-print()
-'''
-END - Detect Domain-specific Content - local
-'''
-
-'''
-Detect Image Types - local
-This example detects an image's type (clip art/line drawing).
-'''
-print("===== Detect Image Types - local =====")
-
-local_image_path_type = "resources/type-image.jpg"
-local_image_type = open(local_image_path_type, "rb")
-local_image_features = [VisualFeatureTypes.image_type]
-
-detect_type_results_local = computervision_client.analyze_image_in_stream(local_image_type, local_image_features)
-
-print("Type of local image:")
-if detect_type_results_local.image_type.clip_art_type == 0:
-    print("Image is not clip art.")
-elif detect_type_results_local.image_type.line_drawing_type == 1:
-    print("Image is ambiguously clip art.")
-elif detect_type_results_local.image_type.line_drawing_type == 2:
-    print("Image is normal clip art.")
-else:
-    print("Image is good clip art.")
-
-if detect_type_results_local.image_type.line_drawing_type == 0:
-    print("Image is not a line drawing.")
-else:
-    print("Image is a line drawing")
-print()
-'''
-END - Detect Image Types - local
-'''
-
-'''
 Detect Objects - local
 This example detects different kinds of objects with bounding boxes in a local image.
 '''
-print("===== Detect Objects - local =====")
+print("===== Detect Objects =====")
 
-local_image_path_objects = "resources/objects.jpg"
-local_image_objects = open(local_image_path_objects, "rb")
+local_image_objects = open(local_image_path, "rb")
 
 detect_objects_results_local = computervision_client.detect_objects_in_stream(local_image_objects)
 
@@ -241,39 +129,12 @@ END - Detect Objects - local
 '''
 
 '''
-Detect Brands - local
-This example detects common brands like logos and puts a bounding box around them.
-'''
-print("===== Detect Brands - local =====")
-
-local_image_path_shirt = "resources/gray-shirt-logo.jpg"
-local_image_shirt = open(local_image_path_shirt, "rb")
-
-local_image_features = ["brands"]
-
-detect_brands_results_local = computervision_client.analyze_image_in_stream(local_image_shirt, local_image_features)
-
-print("Detecting brands in local image: ")
-if len(detect_brands_results_local.brands) == 0:
-    print("No brands detected.")
-else:
-    for brand in detect_brands_results_local.brands:
-        print("'{}' brand detected with confidence {:.1f}% at location {}, {}, {}, {}".format( \
-        brand.name, brand.confidence * 100, brand.rectangle.x, brand.rectangle.x + brand.rectangle.w, \
-        brand.rectangle.y, brand.rectangle.y + brand.rectangle.h))
-print()
-'''
-END - Detect brands - local
-'''
-
-'''
 Generate Thumbnail
-This example creates a thumbnail from both a local and URL image.
+This example creates a thumbnail.
 '''
 print("===== Generate Thumbnail =====")
 
-local_image_path_thumb = "resources/objects.jpg"
-local_image_thumb = open(local_image_path_objects, "rb")
+local_image_thumb = open(local_image_path, "rb")
 
 print("Generating thumbnail from a local image...")
 # Call the API with a local image, set the width/height if desired (pixels)
@@ -281,7 +142,7 @@ print("Generating thumbnail from a local image...")
 thumb_local = computervision_client.generate_thumbnail_in_stream(100, 100, local_image_thumb, True)
 
 # Write the image binary to file
-with open("thumb_local.png", "wb") as f:
+with open("resources/thumb.png", "wb") as f:
     for chunk in thumb_local:
         f.write(chunk)
 
@@ -292,49 +153,17 @@ with open("thumb_local.png", "wb") as f:
 
 print("Thumbnail saved to local folder.")
 print()
-
-
 '''
-Batch Read File, recognize handwritten text - local
-This example extracts text from a handwritten local image, then prints results.
-This API call can also recognize remote image text (shown in next example, Batch Read File - remote).
-'''
-print("===== Batch Read File - local =====")
-
-local_image_handwritten_path = "resources/handwritten_text.jpg"
-local_image_handwritten = open(local_image_handwritten_path, "rb")
-
-# Call API with image and raw response (allows you to get the operation location)
-recognize_handwriting_results = computervision_client.batch_read_file_in_stream(local_image_handwritten, raw=True)
-# Get the operation location (URL with ID as last appendage)
-operation_location_local = recognize_handwriting_results.headers["Operation-Location"]
-# Take the ID off and use to get results
-operation_id_local = operation_location_local.split("/")[-1]
-
-while True:
-    recognize_handwriting_result = computervision_client.get_read_operation_result(operation_id_local)
-    if recognize_handwriting_result.status not in ['notStarted', 'running']:
-        break
-    time.sleep(1)
-
-if recognize_handwriting_result.status == OperationStatusCodes.succeeded:
-    for text_result in recognize_handwriting_result.analyze_result.read_results:
-        for line in text_result.lines:
-            print(line.text)
-            print(line.bounding_box)
-print()
-'''
-END - Batch Read File - local
+END - Generate Thumbnail
 '''
 
 '''
 Recognize Printed Text with OCR - local
 This example will extract, using OCR, printed text in an image, then print results line by line.
 '''
-print("===== Detect Printed Text with OCR - local =====")
+print("===== Detect Printed Text with OCR =====")
 
-local_image_printed_text_path = "resources/printed_text.jpg"
-local_image_printed_text = open(local_image_printed_text_path, "rb")
+local_image_printed_text = open(local_image_path, "rb")
 
 ocr_result_local = computervision_client.recognize_printed_text_in_stream(local_image_printed_text)
 for region in ocr_result_local.regions:
